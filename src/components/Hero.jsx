6e2c9926a959624339b1a436d61ca2f88fcf774c
İkrime Yağbasa <ikrimeyagbasa@Ikrime-MacBook-Pro.local>
2023-09-19 15:43:33 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,11 @@
 import { motion } from "framer-motion"
 import {styles} from '../styles'
 import {ComputersCanvas}  from "./canvas"
+
+
+const AniamtionDev = () => {
+  return 
+}
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
@@ -20,17 +25,19 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-          <motion.dev 
-           animate={{
-            y:[0,24,0]
-           }}
-           transition={{
-            duration:1.5,
-            repeat:Infinity,
-            repeatType:'loop'
-           }}
-           className="w-3 h-3 rounded-full bg-secondary mb-1"
-          />
+            <svg>
+            <motion.dev 
+              animate={{
+                y:[0,24,0]
+              }}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+            </svg>
           </div>
 
         </a>
